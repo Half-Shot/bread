67c228c507c5cba6754d51dbd743ca5ff8e94468
Half-Shot <half-shot@molrams.com>
2014-10-25 20:38:58 +0100

    Removed useless statement from Utilitys

--- modules/Utilitys.php
+++ modules/Utilitys.php
@@ -302,9 +302,6 @@ class Utilitys {
 	 * @param any $ValueToUse Value to use.
 	 */
 	static function EmptySub($CheckMe,$ValueToUse){
-            if(@\is_null($CheckMe)){
-                return $ValueToUse;
-            }
 		if(empty($CheckMe)){
 			return $ValueToUse;
 		}
